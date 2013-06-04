581aa0cee601849054dfd6a22e77b3277920f51e
Marco Balduini <marco.balduini@polimi.it>
2013-06-04 15:56:12 +0200

    - concept flows data server added

---
+++ src/main/java/eu/deib/polimi/city_sensing_server/concept_flows/ConceptFlowsNode.java
@@ -0,0 +1,35 @@
+package eu.deib.polimi.city_sensing_server.concept_flows;
+
+public class ConceptFlowsNode {
+	
+	private String id;
+	private String label;
+	private String group;
+	private long sentiment;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	public long getSentiment() {
+		return sentiment;
+	}
+	public void setSentiment(long sentiment) {
+		this.sentiment = sentiment;
+	}
+
+}
