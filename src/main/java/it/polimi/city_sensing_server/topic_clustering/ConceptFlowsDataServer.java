b087061874beee79fe68b93ec15bd5b8f36a713b
Marco Balduini <marco.balduini@polimi.it>
2014-10-30 10:40:00 +0100

    create topic_clustering service structure

---
+++ src/main/java/it/polimi/city_sensing_server/topic_clustering/ConceptFlowsDataServer.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright 2014 DEIB - Politecnico di Milano
+ *    
+ * Marco Balduini (marco.balduini@polimi.it)
+ * Emanuele Della Valle (emanuele.dellavalle@polimi.it)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package it.polimi.city_sensing_server.topic_clustering;
+
+import java.io.IOException;
+
+import org.restlet.representation.Representation;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ConceptFlowsDataServer extends ServerResource{
+
+	private Logger logger = LoggerFactory.getLogger(ConceptFlowsDataServer.class.getName());
+
+	@Post
+	public void dataServer(Representation rep) throws IOException {
+
+	}
+}
